1307fb3e6e9fbf1b412d30c12f3d1160ab0b1a07
olili27 <timmolili27@gmail.com>
2023-03-21 18:47:10 +0300

    submit pizza bill

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,22 +9,53 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        bill = price + " ";
     }
 
     public int getPrice(){
         return this.price;
     }
 
+    public boolean cheeseAdded = false;
     public void addExtraCheese(){
         // your code goes here
+        if (cheeseAdded) return;
+
+        int sampleBill = Integer.parseInt(bill);
+
+        sampleBill += 80;
+
+        cheeseAdded = true;
+
+        bill = sampleBill + " ";
     }
 
+    public boolean toppingsAdded = false;
     public void addExtraToppings(){
         // your code goes here
+        if(toppingsAdded) return;
+
+        if (!cheeseAdded) addExtraCheese();
+
+        int sampleBill = Integer.parseInt(bill);
+
+        if (isVeg) {
+            sampleBill += 70;
+        } else {
+            sampleBill += 120;
+        }
+
+        toppingsAdded = true;
+
+        bill = sampleBill + " ";
     }
 
     public void addTakeaway(){
         // your code goes here
+        int sampleBill = Integer.parseInt(bill);
+        sampleBill += 20;
+
+        bill = sampleBill + " ";
     }
 
     public String getBill(){
